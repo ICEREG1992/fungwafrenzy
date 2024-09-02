909520af08567f828ba6ff5686e87091bcafad60
William Sullivan <willwill.sullivan@gmail.com>
2024-09-02 01:14:00 -0500

    update icons, add new ipc channels, import fs, fix draggable buttons

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,7 +2,7 @@
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-export type Channels = 'ipc-example' | 'close-app';
+export type Channels = 'ipc-example' | 'close-app' | 'load-userconfig' | 'load-saves' | 'load-impacts';
 
 const electronHandler = {
   ipcRenderer: {
