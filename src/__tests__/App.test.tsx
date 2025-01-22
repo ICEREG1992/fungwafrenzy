0872f27a6f8437787edf6edff7b524f4f27d567d
William Sullivan <willwill.sullivan@gmail.com>
2025-01-22 01:46:58 -0600

    fix test suite

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,8 +2,32 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
-// describe('App', () => {
-//   it('should render', () => {
-//     expect(render(<App />)).toBeTruthy();
-//   });
-// });
+// Mock `window.electron` to prevent ipcRenderer errors
+beforeAll(() => {
+  global.window.electron = {
+    ipcRenderer: {
+      invoke: jest.fn(async (channel, ...args) => {
+        if (channel === 'get-defaultappdatapaths') {
+          return ['path1', 'path2', 'path3']; // Mock response for this channel
+        }
+        return null;
+      }),
+      sendMessage: jest.fn(),
+      on: jest.fn(),
+      once: jest.fn(),
+    },
+  };
+  global.ResizeObserver = class {
+    observe = jest.fn();
+
+    unobserve = jest.fn();
+
+    disconnect = jest.fn();
+  };
+});
+
+describe('App', () => {
+  it('should render', () => {
+    expect(render(<App />)).toBeTruthy();
+  });
+});
